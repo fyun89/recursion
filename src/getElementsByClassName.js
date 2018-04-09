30a776d3daf3c4b2ca9530018d37ad3505d74f96
fyun89 <fyun89@gmail.com>
2018-04-09 14:38:24 -0700

    stringifyJSON: now capable of conversion of array to string

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,12 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
+  var matchingElements = []
+  for (className)
+  var dividedHTMLString = className.slice().split(' ')
+  if (dividedHTMLString )
+  	//push matching items to matchingElements array	
   // your code here
+  //return matchingElements
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,22 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (Array.isArray(obj) === true){ // your code goes here
+  	var arrayResult = '['
+  	//console.log(obj[1])
+  	for (var i = 0; i < obj.length; i++){
+  		if (i < obj.length-1){
+  			//console.log(obj[i].toString())
+  			arrayResult += obj[i].toString() + ', '
+  		}else if (i === obj.length - 1){
+  			arrayResult += obj[i].toString()
+  		} 
+  	}
+  	console.log(arrayResult+']')
+  	return arrayResult + ']'
+  }else if (typeof obj === object){
+
+  }else{
+  	return toString(obj)
+  }
 };
